e034c888594f5999d6b73cf5f9473cfa3aa7aa99
GoGyoonGa <ouna2@naver.com>
2023-03-11 19:36:30 +0900

    2023-03-11

--- Morailty_p/Assets/Scripts/1game/workChairScript.cs
+++ Morailty_p/Assets/Scripts/1game/workChairScript.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class workChairScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject workChairIn; 
+    public GameObject workChairOut;
+    private bool state = false; //in
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            if(state == true) //out
+            {
+                workChairIn.gameObject.SetActive(false);
+                workChairOut.gameObject.SetActive(true);
+                state = false; //in
+            }
+            else
+            {
+                workChairIn.gameObject.SetActive(true);
+                workChairOut.gameObject.SetActive(false);
+                state = true; //out
+            }
+        }
     }
 }
